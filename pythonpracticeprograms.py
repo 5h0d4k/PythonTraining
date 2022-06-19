3c03ecb44169983bb68c768a5d8ea62e10f31dc5
5h0d4k <5h0d4k@gmail.com>
2022-06-20 00:25:34 +0100

    updated practiceprograms2

--- pythonpracticeprograms.py
+++ pythonpracticeprograms.py
@@ -1,7 +1,9 @@
-name = 'KK'
+name = "Kamalkanth Behera"
 age = 44
 #
-print(name+ ' ' +'is his name')
-print('He is ' ,age, 'years old')
-print('His name  is ' +name +  "\n" 'and he is' ,str(age), 'years old')
-print (name + "\n" + str(age))
+print(name + " " + "is his name")
+print("He is ", age, "years old")
+print("His name  is " + name + "\n" "and he is", str(age), "years old")
+print(name + "\n" + str(age))
+print(name[6])
+print(name.lower())
