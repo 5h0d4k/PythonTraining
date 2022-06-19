bb944e90660059a2b6be3e85383312af83efa139
5h0d4k <5h0d4k@gmail.com>
2022-06-20 00:06:04 +0100

    practiceprograms2

---
+++ pythonpracticeprograms.py
@@ -0,0 +1,7 @@
+name = 'KK'
+age = 44
+#
+print(name+ ' ' +'is his name')
+print('He is ' ,age, 'years old')
+print('His name  is ' +name +  "\n" 'and he is' ,str(age), 'years old')
+print (name + "\n" + str(age))
